14bef71c5d6ef970dfd729e90fc2408ffae477ad
laegel <laegel@selper.co.kr>
2023-07-31 12:14:10 +0900

    JVM 게시

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["Spring Security"],
         "url": "/spring%20security/ch0/",
         "teaser": null
+      },{
+        "title": "[JVM] JVM 총정리 - (1)",
+        "excerpt":"JVM 은 사용자 컴퓨터 운영체제에 맞는 JRE 에 포함되어 있는 소프트웨어로, 바이트 코드로 변환된 자바 코드를 실행시켜주는 역할을 한다. 또한 자바 프로그램을 실행함에 있어 .class 파일에 대한 유효성 검사와 메모리 관리, GC 등의 작업을 수행하기 때문에 매우 중요한 소프트웨어라고 할 수 있다. 먼저 JVM 내부 구조를 살펴보자. 1. JVM 내부구조...","categories": ["Java"],
+        "tags": ["Java"],
+        "url": "/java/ch0/",
+        "teaser": null
       }]
