e443294c9ef707be013489099ce2282fcc126ca0
laegel <laegel@selper.co.kr>
2023-08-07 14:23:09 +0900

    JVM 게시

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,14 +6,14 @@ var store = [{
         "teaser": null
       },{
         "title": "[JVM] JVM 총정리 - 클래스 로더(1)",
-        "excerpt":"JVM 은 사용자 컴퓨터 운영체제에 맞는 JRE 에 포함되어 있는 소프트웨어로, 바이트 코드로 변환된 자바 코드를 실행시켜주는 역할을 한다. 또한 자바 프로그램을 실행함에 있어 .class 파일에 대한 유효성 검사와 메모리 관리, GC 등의 작업을 수행하기 때문에 매우 중요한 소프트웨어라고 할 수 있다. 먼저 JVM 내부 구조를 살펴보자. 1. JVM 내부구조...","categories": ["Java"],
+        "excerpt":"JVM 은 사용자 컴퓨터 운영체제에 맞는 JRE 에 포함되어 있는 소프트웨어로, 바이트 코드로 변환된 자바 코드를 실행시켜주는 역할을 한다. 또한 자바 프로그램을 실행함에 있어 .class 파일에 대한 유효성 검사와 메모리 관리, GC 등의 작업을 수행하기 때문에 매우 중요한 소프트웨어라고 할 수 있다. 먼저 JVM 내부 구조를 살펴보자. 1. JVM 내부구조...","categories": ["Java","JVM"],
         "tags": ["Java","JVM"],
-        "url": "/java/ch0/",
+        "url": "/java/jvm/ch0/",
         "teaser": null
       },{
         "title": "[JVM] JVM 총정리 - 런타임 영역(2)",
-        "excerpt":"런타임 영역 은 JVM 이 런타임 환경에서 자바 프로그램을 수행할 때 OS 로부터 할당받는 메모리 영역이다. 이 메모리 영역은 논리적으로 다음과 같이 5가지 영역으로 구분한다. Method Area Heap Area PC Register Native Method Stack JVM Stack Method Area Method Area 는 JVM 벤더마다 다르게 구현되어 있다. 그러나 거의 모든 운영체제에서는...","categories": ["Java"],
+        "excerpt":"런타임 영역 은 JVM 이 런타임 환경에서 자바 프로그램을 수행할 때 OS 로부터 할당받는 메모리 영역이다. 이 메모리 영역은 논리적으로 다음과 같이 5가지 영역으로 구분한다. Method Area Heap Area PC Register Native Method Stack JVM Stack Method Area Method Area 는 JVM 벤더마다 다르게 구현되어 있다. 그러나 거의 모든 운영체제에서는...","categories": ["Java","JVM"],
         "tags": ["Java","JVM"],
-        "url": "/java/ch1/",
+        "url": "/java/jvm/ch1/",
         "teaser": null
       }]
